ee87a04cc59139a1cbb5db9edb901ac3eff4f82b
boronin_1993@mail.ru <boronin_1993@mail.ru>
2023-01-15 16:30:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ConsoleApp14
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp14;
+using static System.Console;
+using (ApplicationContext db = new ApplicationContext())
+{
+    //создаем два объекта
+    User Kirill = new User { Name = "Кирилл", Age = 16 };
+    User Semen = new User { Name = "Семён", Age = 14 };
+
+    //добавляем их в бд
+    db.Users.Add(Kirill);
+    db.Users.Add(Semen);
+    db.SaveChanges();
+    WriteLine("Объекты успешно сохранены");
+
+    //получаем объекты из бд и выводим на консоль
+    var users=db.Users.ToList();
+    WriteLine("Список объектов");
+    foreach(User u in users)
+    {
+        WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
